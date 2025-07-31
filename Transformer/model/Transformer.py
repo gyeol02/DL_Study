6ef714847620141cecf2fe6f2c5694d846597df6
고결 <gogyeol056@gmail.com>
2025-07-31 13:06:09 +0900

    update[3] Transformer.py

--- Transformer/model/Transformer.py
+++ Transformer/model/Transformer.py
@@ -35,29 +35,29 @@ class Transformer(nn.Module):
         B, tgt_len = tgt.shape
 
         if src_mask is None:
-            src_mask = create_padding_mask(src)  # (B, 1, 1, src_len)
+            src_mask = create_padding_mask(src)                               # (B, 1, 1, src_len)
 
         if tgt_mask is None:
-            padding_mask = create_padding_mask(tgt)  # (B, 1, 1, tgt_len)
+            padding_mask = create_padding_mask(tgt)                           # (B, 1, 1, tgt_len)
             look_ahead_mask = create_look_ahead_mask(tgt_len).to(tgt.device)  # (tgt_len, tgt_len)
             look_ahead_mask = look_ahead_mask.unsqueeze(0).unsqueeze(1)       # (1, 1, tgt_len, tgt_len)
-            tgt_mask = padding_mask & look_ahead_mask  # (B, 1, tgt_len, tgt_len)
+            tgt_mask = padding_mask & look_ahead_mask                         # (B, 1, tgt_len, tgt_len)
 
         if memory_mask is None:
-            memory_mask = create_padding_mask(src)  # (B, 1, 1, src_len)
+            memory_mask = create_padding_mask(src)                            # (B, 1, 1, src_len)
 
         # 1. Embedding + Positional Encoding
         src_emb = self.pos_encoding(self.src_embedding(src) * (self.d_model ** 0.5))  # (B, src_len, D)
         tgt_emb = self.pos_encoding(self.tgt_embedding(tgt) * (self.d_model ** 0.5))  # (B, tgt_len, D)
 
         # 2. Encoder
-        memory = self.encoder(src_emb, src_mask)  # (B, src_len, D)
+        memory = self.encoder(src_emb, src_mask)                              # (B, src_len, D)
 
         # 3. Decoder
-        out = self.decoder(tgt_emb, memory, tgt_mask, memory_mask)  # (B, tgt_len, D)
+        out = self.decoder(tgt_emb, memory, tgt_mask, memory_mask)            # (B, tgt_len, D)
 
         # 4. Output Projection to vocab
-        logits = self.output_layer(out)  # (B, tgt_len, vocab_size)
+        logits = self.output_layer(out)                                       # (B, tgt_len, vocab_size)
         return logits
     
 """ Positional Encoding"""
@@ -68,7 +68,7 @@ class Positional_Encoding(nn.Module):
 
         # Positional encoding matrix 초기화 (max_len, d_model)
         pe = torch.zeros(max_len, d_model)
-        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)   # (max_len, 1)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)                         # (max_len, 1)
         div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))  # (d_model/2,)
 
         pe[:, 0::2] = torch.sin(position * div_term)   # 짝수 인덱스
